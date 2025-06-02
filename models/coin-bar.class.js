f66c8453e29c8889f626d357e999eaa4bde06d44
Vasilios I. <vasilios.indopoulos@gmx.de>
2025-06-02 15:04:22 +0200

    added Coins
    added Coins to Map

--- models/coin-bar.class.js
+++ models/coin-bar.class.js
@@ -13,7 +13,7 @@ class CoinBar extends DrawableObject {
         super();
         this.loadImages(this.IMAGES);
         this.x = 20;
-        this.y = 30;
+        this.y = 45;
         this.width = 200;
         this.height = 60;
         this.setCoins(0);
---
+++ models/coin.class.js
@@ -0,0 +1,22 @@
+class Coin extends movableObject {
+
+    y = 130;
+    IMAGES_COIN = [
+        'img/8_coin/coin_1.png',
+        'img/8_coin/coin_2.png',
+    ];
+
+    constructor() {
+        super();  // Superklasse korrekt aufrufen
+        this.loadImage(this.IMAGES_COIN[0]);  // Einzelbild
+        this.loadImages(this.IMAGES_COIN);    // Array für Animation
+        this.animate();
+        this.x = 450 + Math.random() * 1500;
+    }
+
+    animate() {
+        setInterval(() => {
+            this.playAnimation(this.IMAGES_COIN);
+        }, 200);
+    }
+}
